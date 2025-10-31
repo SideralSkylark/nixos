c053e3c8046fe46d888607599ab79751c1250370
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2025-10-31 17:21:20 +0200

    changed pallete to kanagawa

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -5,7 +5,7 @@
 
     image = null;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
 
     fonts = {
       monospace = {
