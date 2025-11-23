456bfde66e70d40462df1becc99d42fc33c4898e
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2025-11-23 20:10:37 +0200

    gaming role working

--- modules/role/gaming/default.nix
+++ modules/role/gaming/default.nix
@@ -1,5 +1,33 @@
+{ pkgs, ... }:
+
 {
     imports = [
        ./packages.nix 
     ];
+
+    hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+
+    extraPackages = with pkgs; [
+        mesa_drivers
+        vulkan-loader
+        vulkan-validation-layers
+        vaapiVdpau
+        libvdpau-va-gl
+    ];
+
+    extraPackages32 = with pkgs; [
+        mesa_drivers
+        vulkan-loader
+    ];
+  };
+
+  # --- Steam ---
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+    };
+
 }
--- modules/role/gaming/packages.nix
+++ modules/role/gaming/packages.nix
@@ -3,5 +3,11 @@
 {
     environment.systemPackages = with pkgs; [
         hydralauncher
+        heroic
+        lutris
+        bottles
+        wineWowPackages.full
+        winetricks
+        protonup-qt
     ]; 
 }
