b39b79cfb49b82bcde1b7a0ce5e12a705ce829b5
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-02-14 15:46:42 +0200

    ssh keys load on start with ssh-agent

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,6 +1,15 @@
 {
-    services.openssh.enable = true;
-    programs.ssh = {
-        startAgent = true;      
-    };
+  services.openssh.enable = true;
+  programs.ssh = {
+    startAgent = true;
+
+    # iclude github ssh key on boot
+    extraConfig = ''
+      Host github.com
+        HostName github.com
+        User git
+        IdentityFile ~/.ssh/id_ed25519
+        AddKeysToAgent yes
+    '';
+  };
 }
