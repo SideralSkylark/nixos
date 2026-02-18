f8bed40740c478252bde6313d655d1cfb899cfeb
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-02-18 20:41:27 +0200

    removed custom bin and bat

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -1,17 +1,6 @@
 {
-  programs.bat = {
-    enable = true;
-    config = {
-      paging = "never";
-      style = "numbers";
-    };
-  };
-
   programs.bash = {
     enable = true;
-    shellAliases = {
-      kn = "~/.local/bin/kn";
-    };
   };
 
   programs.starship.enable = true;
