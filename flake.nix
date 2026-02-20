b401aea6506517a0a18de7212c582808685456e9
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-02-20 20:18:40 +0200

    ref: reduced reduncancy in flake

--- flake.nix
+++ flake.nix
@@ -1,73 +1,75 @@
 {
-    description = "NixOs configuration";
+  description = "NixOS & Home Manager configuration";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
 
-        home-manager = {
-            url = "github:nix-community/home-manager/release-25.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        stylix = {
-            url = "github:nix-community/stylix/release-25.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        nixvim = {
-	        url = "github:nix-community/nixvim/nixos-25.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs: {
-
-        nixosConfigurations = {
-            laptop = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-
-                modules = [
-                    ./hosts/laptop/configuration.nix
-                    inputs.stylix.nixosModules.stylix
-                    home-manager.nixosModules.home-manager {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.skylark =  { ... }: {
-                                imports = [
-                                    ./home/skylark.nix
-                                    ./home/modules/hyprland
-                                ];
-                            };
-                            backupFileExtension = "backup";
-                            extraSpecialArgs = { inherit nixvim; };
-                        };
-                    }
-                ];
-            };
-
-            nixos = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-
-                modules = [
-                    ./hosts/nixos/configuration.nix
-                    inputs.stylix.nixosModules.stylix
-                    home-manager.nixosModules.home-manager {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.skylark =  { ... }: {
-                                imports = [
-                                    ./home/skylark.nix
-                                    ./home/modules/hyprland
-                                ];
-                            };
-                            backupFileExtension = "backup";
-                            extraSpecialArgs = { inherit nixvim; };
-                        };
-                    }
-                ];
-            };
-        };
+    stylix = {
+      url = "github:nix-community/stylix/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, stylix, nixvim, ... }@inputs:
+  let
+    system = "x86_64-linux";
+
+    mkHost = hostPath:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          hostPath
+          stylix.nixosModules.stylix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              users.skylark = { ... }: {
+                imports = [
+                  ./home/skylark.nix
+                  ./home/modules/hyprland
+                ];
+              };
+
+              backupFileExtension = "backup";
+              extraSpecialArgs = { inherit nixvim; };
+            };
+          }
+        ];
+      };
+  in
+  {
+    # -------- NixOS machines --------
+    nixosConfigurations = {
+      laptop = mkHost ./hosts/laptop/configuration.nix;
+      nixos  = mkHost ./hosts/nixos/configuration.nix;
+    };
+
+    # -------- Fedora / Standalone HM --------
+    homeConfigurations = {
+      skylark = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        modules = [
+          ./home/skylark.nix
+          ./home/modules/kde   # futuro módulo KDE
+        ];
+
+        extraSpecialArgs = { inherit nixvim; };
+      };
+    };
+  };
 }
