2df31a7066664967a3ecf4e33d4818b23d944233
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-03-08 20:35:04 +0200

    fix: remember user

--- modules/desktop/session.nix
+++ modules/desktop/session.nix
@@ -9,7 +9,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session --cmd Hyprland";
         user = "greeter";
       };
     };
