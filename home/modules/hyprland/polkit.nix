c1212085fe1b74a3afbbb574a1d9341be70c2773
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-03-09 21:11:21 +0200

    polkit activation in hyprland

--- home/modules/hyprland/polkit.nix
+++ home/modules/hyprland/polkit.nix
@@ -4,17 +4,18 @@
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
     Unit = {
       Description = "Polkit authentication agent for Hyprland";
-      After = [ "hyprland-session.target" ]; # ← muda aqui
-      PartOf = [ "hyprland-session.target" ]; # ← e aqui
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
     };
+
     Service = {
       Type = "simple";
       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
       Restart = "on-failure";
-      RestartSec = 3;
     };
+
     Install = {
-      WantedBy = [ "hyprland-session.target" ]; # ← e aqui
+      WantedBy = [ "graphical-session.target" ];
     };
   };
 }
