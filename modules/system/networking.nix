cd9bed2341b10480a27889ac265a60cb22f8f041
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-03-17 19:25:20 +0200

    fix: dns auto resolution

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,28 +1,11 @@
-{ ... }:
 {
-  networking.wireless.iwd = {
-    enable = true;
-    settings = {
-      General = {
-        EnableNetworkConfiguration = true;
-        AddressRandomization = "network"; 
-      };
-      Station.RoamingEnabled = true;
-    };
-  };
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
 
-  networking.useNetworkd = true;
-  networking.useDHCP = false;
-  systemd.network = {
-    enable = true;
-    networks."10-ethernet" = {
-      matchConfig.Type = "ether";
-      networkConfig.DHCP = "yes";
-      dhcpV4Config.RouteMetric = 100; #perfer ethernet over wifi
-    };
+  networking.networkmanager.wifi = {
+    powersave = true;
+    macAddress = "stable-ssid";
   };
-
-  networking.networkmanager.enable = false;
 
   networking.firewall = {
     enable = true;
