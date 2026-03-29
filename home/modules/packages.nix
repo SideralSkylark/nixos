ebd1414812f9f824fd223cebd38058602b4380e5
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-03-29 15:50:29 +0200

    gpg for encription

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -1,24 +1,19 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    fastfetch
-
-    btop
-
-    git
-    ripgrep
-    fd
-    fzf
-
-    curl
+    fastfetch #info fetcher
+    btop #process monitor
+    git #version controll
+    ripgrep #faster grep
+    fd #faster find
+    fzf #fuzzy find
+    curl 
     wget
-
     zip
     unzip
     p7zip
     unrar
-
     openssh
-    man-pages
+    man-pages 
   ];
 }
--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./audio.nix
     ./power.nix
+    ./gpg.nix
     ./ssh.nix
     ./storage.nix
   ];
