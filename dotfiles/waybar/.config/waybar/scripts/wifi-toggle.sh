4a8f3a81eb22bc44ccfff0ede5fa69c416bf56e7
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-04-18 17:01:49 +0200

    better light mode for nixvim

--- dotfiles/waybar/.config/waybar/scripts/wifi-toggle.sh
+++ dotfiles/waybar/.config/waybar/scripts/wifi-toggle.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-IFACE=$(iwctl device list | awk '/wlan|wlp/ {print $2; exit}')
-ADAPTER=$(iwctl device "$IFACE" show | awk '/Adapter/ {print $NF; exit}')
+STATE=$(nmcli radio wifi)
 
-STATE=$(iwctl device "$IFACE" show | awk '/Powered/ {print $NF}')
-
-if [ "$STATE" = "on" ]; then
-    iwctl station "$IFACE" disconnect
-    iwctl adapter "$ADAPTER" set-property Powered off
+if [ "$STATE" = "enabled" ]; then
+    nmcli radio wifi off
     notify-send -a "WiFi" -u low -t 3000 "󰤮 WiFi" "Disabled"
 else
-    iwctl adapter "$ADAPTER" set-property Powered on
-    iwctl station "$IFACE" scan
+    nmcli radio wifi on
     notify-send -a "WiFi" -u low -t 3000 "󰤨 WiFi" "Enabled"
 fi
