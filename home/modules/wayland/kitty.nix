1d8c51aa73dfc0987ee8dd7642fc908b133efc13
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-04-23 21:36:25 +0200

    merged kitty conf into a single file

--- home/modules/wayland/kitty.nix
+++ home/modules/wayland/kitty.nix
@@ -1,6 +1,5 @@
 {
   xdg.configFile = {
     "kitty/kitty.conf".source = ../../../dotfiles/kitty/.config/kitty/kitty.conf;
-    "kitty/current-theme.conf".source = ../../../dotfiles/kitty/.config/kitty/current-theme.conf;
   };
 }
