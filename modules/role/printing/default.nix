7bee37c6387b1eacc8e9b3c18428d0a6a9cacf97
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-05-15 17:34:44 +0200

    fix: compact printing role

--- modules/role/printing/default.nix
+++ modules/role/printing/default.nix
@@ -1,6 +1,28 @@
+{ pkgs, ... }:
 {
-  imports = [
-    ./printing.nix
-    ./scanning.nix
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.hplip
+      pkgs.epson-escpr
+    ];
+  };
+
+  hardware.sane = {
+    enable = true;
+    extraBackends = [
+      pkgs.hplip # HP multifunction
+      pkgs.epkowa # Epson multifunction
+    ];
+  };
+
+  services.udev.packages = [ pkgs.sane-backends ];
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.skanlite # scanning frontend
   ];
+
+  users.groups.scanner = { };
+  users.groups.lp = { };
+  users.groups.lpadmin = { };
 }
