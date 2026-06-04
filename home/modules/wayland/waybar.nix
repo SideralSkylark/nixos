939430ad9f6787e3e8fb179d21fb8d17cf888f75
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-06-04 11:07:36 +0200

    fix: typo

--- home/modules/wayland/waybar.nix
+++ home/modules/wayland/waybar.nix
@@ -5,11 +5,11 @@
   ...
 }:
 {
-  xdg.configfile = {
+  xdg.configFile = {
     "waybar/config.jsonc".source = ../../../dotfiles/waybar/.config/waybar/config.jsonc;
     "waybar/style.css".source = ../../../dotfiles/waybar/.config/waybar/style.css;
     "waybar/scripts".source = ../../../dotfiles/waybar/.config/waybar/scripts;
   };
 
-  home.packages = lib.mkIf config.wayland.mannageWaybarPackage [ pkgs.waybar ];
+  home.packages = lib.mkIf config.wayland.manageWaybarPackage [ pkgs.waybar ];
 }
