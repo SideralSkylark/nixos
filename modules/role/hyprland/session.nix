7d9408aa40d1600786fb0a498f0cbae3ee1f7741
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-06-16 17:17:30 +0200

    fix: unecessay user from tuigreet session nixos should default to greetd

--- modules/role/hyprland/session.nix
+++ modules/role/hyprland/session.nix
@@ -10,7 +10,6 @@
     settings = {
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session --cmd 'uwsm start hyprland-uwsm.desktop'";
-        user = "greeter";
       };
     };
   };
