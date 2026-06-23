4ec1346280262bd567e0ac333c96e7b9ae889a2a
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-06-23 21:28:44 +0200

    updated deprecated packages

--- modules/role/gaming/default.nix
+++ modules/role/gaming/default.nix
@@ -5,7 +5,7 @@
     heroic # Epic, GOG, and Amazon Games launcher
     lutris # Open gaming platform for Linux
     bottles # Run Windows software on Linux
-    wineWowPackages.full # WINE with full compatibility
+    wineWow64Packages.full # WINE with full compatibility
     winetricks # Helper script for WINE
     protonup-qt # Manage Proton-GE and Luxtorpeda versions
   ];
