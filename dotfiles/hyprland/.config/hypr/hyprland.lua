5b242dc44bfbd4858772ead1b7a3557b2afe6161
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-09-12 12:48:21 +0200

    reduced unecessary file

--- dotfiles/hyprland/.config/hypr/hyprland.lua
+++ dotfiles/hyprland/.config/hypr/hyprland.lua
@@ -1,4 +1,6 @@
-require("startup")
+hl.on("hyprland.start", function()
+    hl.exec_cmd("noctalia --daemon")
+end)
 
 -- ── PROGRAMS ────────────────────────────────────────────────
 local terminal         = "ghostty"
